d20e8beb4d92d5c9e4950697c2d900e65872ac43
Md. Ashraf-ul-Huque <shaonashraf@gmail.com>
2018-08-12 16:57:28 +0600

    Made ui for the UserFormComponent

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Modules
+import { CardModule } from 'primeng/card';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
-import { SigninComponent } from '../shared/signin/signin.component';
 import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+
+// Components
+import { SigninComponent } from '../shared/signin/signin.component';
 import { AuthenticatedUserComponent } from './authenticated-user/authenticated-user.component';
 import { HomeLayoutComponent } from '../shared/home-layout/home-layout.component';
 import { HeaderComponent } from '../shared/header/header.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
-  imports: [CommonModule, AuthRoutingModule, FormsModule],
-  declarations: [AuthenticatedUserComponent]
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+  ],
+  declarations: [AuthenticatedUserComponent, UserFormComponent]
 })
 export class AuthModule {}

b22d2cb5d5219058b7b9c2b39b568e3f61b21f17
Raihan Zaman <raihan.zaman@amadeyr.org>
2018-08-13 16:58:17 +0600

    make row per page functionality

--- src/app/auth/service/user.service.ts
+++ src/app/auth/service/user.service.ts
@@ -10,8 +10,8 @@ export class UserService {
   users(event) {
     // @ts-ignore
     const params = new HttpParams()
-      .set('page', (event.first / event.rows) + 1);
-      // .set('sort', SortOn);
+      .set('page', (event.first / event.rows) + 1)
+      .set('per_page', event.rows);
     return this.http.get<Paginate>('/user', {params: params});
   }
   store() {}

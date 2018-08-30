927676e248b708a37a2ad6179f7e39bb7a6d453a
Raihan Zaman <raihan.zaman@amadeyr.org>
2018-08-30 17:50:17 +0600

    working on the artwork table

--- src/app/sampling/sampling-routing.module.ts
+++ src/app/sampling/sampling-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ArtworkFormComponent} from './artwork-form/artwork-form.component';
+import {TableWorkComponent} from './table-work/table-work.component';
 
 const routes: Routes = [
   { path: 'artwork', component: ArtworkFormComponent },
+  { path: 'tablework', component: TableWorkComponent },
 ];
 
 @NgModule({

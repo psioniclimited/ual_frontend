8cafc4179f79a136febcb08b6de924dfedceb523
Raihan Zaman <raihan.zaman@amadeyr.org>
2018-09-07 03:50:18 +0600

    created artowrk_list comoponent

---
+++ src/app/sampling/artwork-list/artwork-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artwork-list',
+  templateUrl: './artwork-list.component.html',
+  styleUrls: ['./artwork-list.component.scss']
+})
+export class ArtworkListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
